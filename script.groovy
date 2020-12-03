55883b23ec029e73216716b2dd6d4bfb5a004b57
gyzi <egz7li@gmail.com>
2020-12-03 13:24:51 +0200

    Create script.groovy
    adding groovy  script

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp(){
+    echo " This is Build Part "
+}
+
+def testApp(){
+    echo " This is test Part "
+}
+
+def deployApp(){
+    echo " This is deploy Part and version ${VER} "
+}
+
+return this
